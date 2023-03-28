879e1be63d38bdc3a206d6b47a078cef48865e5e
Lucas de Brito <devlucasmail@gmail.com>
2023-03-27 21:01:31 -0300

    Typography # feat: Change font size and add padding to texts

--- app/src/components/portfolio/about/index.jsx
+++ app/src/components/portfolio/about/index.jsx
@@ -26,7 +26,7 @@ export default function About() {
                 </Col>
                 <Col xs={12} lg={7}>
                     <Fade>
-                        <p className="about-text mb-0">
+                        <p className="sections-text mb-0">
                             Meu nome é Lucas de Brito, tenho {(new Date()).getUTCFullYear() - 2000} anos e, na verdade,
                             a imagem que vc está vendo me representa bastante, amo basicamente tudo que tem nela, café, plantas e
                             tecnologia.
