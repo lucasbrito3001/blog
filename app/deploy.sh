0f26ca601246f230376ddc117ce88330d145d6f3
Lucas de Brito <devlucasmail@gmail.com>
2023-04-05 09:31:33 -0300

    config github actions

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 echo 'Going to the app directory'
 cd /var/app/blog
+echo 'Done'\n
 
 echo 'Going to the main branch'
 git checkout main
+echo 'Done'\n
 
 echo 'Pulling the branch changes'
 git pull origin main
+echo 'Done'\n
 
 echo 'Changing to the app directory'
 cd app
+echo 'Done'\n
 
 echo 'Installing dependencies'
 yarn
+echo 'Done'\n
 
 echo 'Building application'
 yarn build
+echo 'Done'\n
 
 echo 'Copying the builded code to public_html directory'
 cp -r dist/. /var/www/lucasdbrito.com/public_html
+echo 'Done'\n
