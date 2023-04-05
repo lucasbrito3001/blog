e2227a68e633baacad8deb0e4e42f32d2ad681d6
Lucas de Brito <devlucasmail@gmail.com>
2023-04-05 08:49:19 -0300

    Shell script to deploy

---
+++ app/deploy.sh
@@ -0,0 +1,20 @@
+echo 'Going to the app directory'
+cd /var/app/blog
+
+echo 'Going to the main branch'
+git checkout main
+
+echo 'Pulling the branch changes'
+git pull origin main
+
+echo 'Changing to the app directory'
+cd app
+
+echo 'Installing dependencies'
+yarn
+
+echo 'Building application'
+yarn build
+
+echo 'Copying the builded code to public_html directory'
+cp -r dist/. /var/www/lucasdbrito.com/public_html
