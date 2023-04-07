12c4f532c7fa0491ce1296169933092618371eda
Lucas de Brito <devlucasmail@gmail.com>
2023-04-07 16:50:04 -0300

    New post # feat: How to automate your deploy with GitHub Actions

--- app/src/components/post/unorderedList/index.jsx
+++ app/src/components/post/unorderedList/index.jsx
@@ -3,7 +3,7 @@ import "./styles.scss"
 export default function UnorderedList({list}) {
     return (
         <ul className="unordered-list">
-            {list.map((item, idx) => <li className="unordered-list-item" key={idx}>{item}</li>)}
+            {list.map((item, idx) => <li className="unordered-list-item" key={idx} dangerouslySetInnerHTML={{ __html: item }}></li>)}
         </ul>
     )
 }
